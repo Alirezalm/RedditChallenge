28019513aa031e5fe29d6b5c9bc43379fc34dd58
alireza <alireza.lm69@gmail.com>
2021-02-18 20:29:26 -0300

    CommentViewSet (initial) and its url implemented

--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -12,62 +12,7 @@ from topic.my_permissions import IsAuthorOrReadOnly
 from topic.serializers import TopicSerializer
 
 
-class CommentListViews(APIView):
+class CommentViewSet(viewsets.ModelViewSet):
 
-    def get_permissions(self):
-
-        if self.request.method == 'GET':
-            self.permission_classes = [AllowAny]
-        else:
-            self.permission_classes = [IsAuthenticated,]
-
-        return super().get_permissions()
-
-    def get(self, request, url_name, pk):
-
-        topic = Topic.objects.get(url_name=url_name)
-        post = topic.post_set.get(pk = pk)
-        comments = post.comment_set.all()
-
-        serializer = CommentSerializer(comments, many=True)
-
-        return Response(serializer.data)
-
-    def post(self, request, url_name, pk):
-
-        serializer = CommentSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CommentDetailViewSet(APIView):
-    permission_classes = [IsAuthorOrReadOnly, ]
-
-    def get_object(self, url_name, pk_post, pk_comment):
-        try:
-            topic = Topic.objects.get(url_name=url_name)
-            post = topic.post_set.get(pk=pk_post)
-            return post.comment_set.get(pk=pk_comment)
-        except Topic.DoesNotExist:
-            raise Http404
-
-    def get(self, request, url_name, pk_post, pk_comment, format=None):
-        comment = self.get_object( url_name, pk_post, pk_comment)
-        serializer = CommentSerializer(comment)
-        return Response(serializer.data)
-
-    def put(self, request,url_name, pk_post, pk_comment, format=None):
-        comment = self.get_object(url_name, pk_post, pk_comment)
-        serializer = CommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request,url_name, pk_post, pk_comment, format=None):
-        comment = self.get_object(url_name, pk_post, pk_comment)
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
--- topic/urls.py
+++ topic/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_nested import routers
 
 from comment.models import Comment
-from comment.views import CommentListViews, CommentDetailViewSet
+from comment.views import CommentViewSet
 from post.views import PostViewSet
 
 from topic import views
@@ -14,6 +14,9 @@ router.register('topics', viewset=views.TopicViewSet, basename='topics')
 post_router = routers.NestedDefaultRouter(router, 'topics', lookup='topic')
 post_router.register('posts', PostViewSet, basename='posts')
 
-urlpatterns = router.urls + post_router.urls
+comment_router = routers.NestedDefaultRouter(post_router, 'posts', lookup='post')
+comment_router.register('comments', CommentViewSet, basename='comments')
+
+urlpatterns = router.urls + post_router.urls + comment_router.urls
 
 
